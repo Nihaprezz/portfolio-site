cd4e73427e281b488436e79799bafca00d85cad8
nihaprezz <serg13prez@outlook.com>
2020-01-08 18:53:49 -0500

    sample header with responsive action done

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from './containers/Navbar'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        This is the APP js file
+        < Navbar />
       </header>
     </div>
   );
---
+++ portfolio-site/src/containers/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+class Navbar extends React.Component {
+
+    render(){
+        return (
+            <React.Fragment >
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="/">{'< Sergio />'}</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul> {/* Bootstrap needs this tag to right align my other navbar links for some reason */}
+
+                    <ul class="navbar-nav my-2 my-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Projects</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Other</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Navbar
