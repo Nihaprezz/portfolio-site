ab7108a8488e1ca2fbb420e9209cbd4f0f71f177
nihaprezz <serg13prez@outlook.com>
2020-01-09 13:59:09 -0500

    adding pictures and some responsive on the projects page

--- portfolio-site/src/Projects/ProjectsPage.js
+++ portfolio-site/src/Projects/ProjectsPage.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import "../styles/projects_page.scss"
 
 class ProjectsPage extends React.Component {
 
     render(){
         return (
-            <div> This is the projects page </div>
+            <div className="projects-page">
+                <div className="project-card">
+                    <p>The Game Couch</p>
+                    <div className="gc-img project-img"></div>
+                </div>
+
+                <div className="project-card">
+                    <p>Movie Hangout</p>
+                    <div className="mh-img project-img"></div>
+                </div>
+
+                <div className="project-card">
+                    <p>BrewQueue</p>
+                    <div className="bq-img project-img"></div>
+                </div>
+
+                <div className="project-card">
+                    <p>Burger Diner</p>
+                    <div className="bd-img project-img"></div>
+                </div>
+            </div>
         )
     }
 }
