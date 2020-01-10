638055b6da91918af12813edc4afb9fc69a7c47b
nihaprezz <serg13prez@outlook.com>
2020-01-10 16:40:06 -0500

    finished syling home page

--- portfolio-site/src/Home/HomePage.js
+++ portfolio-site/src/Home/HomePage.js
@@ -1,15 +1,30 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import "../styles/home_page.scss"
 
 class HomePage extends React.Component {
 
     render (){
         return (
-            <div>
-                <div>
-                    <h1>Hi, my name is <span>Sergio Perez</span></h1>
-                    <h2>I am a Fullstack Web Developer.</h2>
-                    <button><Link to="/projects">Projects</Link></button>
+            <div className="home-page">
+                <div className="home-page-details-container">
+                    <p className="intro">Hi, my name is <span>Sergio Perez</span>.</p>
+
+                    <p className="i-am">I am a Fullstack Web Developer.</p>
+
+                    <Link to="/projects"><button>Projects</button></Link>
+
+                    <div className="home-icons">
+                        {/* eslint-disable-next-line */}
+                        <a target="_blank" href="https://github.com/Nihaprezz">
+                            <i class="fab fa-github"></i>
+                        </a>
+
+                        {/* eslint-disable-next-line */}
+                        <a target="_blank" href="https://www.linkedin.com/in/sergio-perez-63928919b/">
+                        <i class="fab fa-linkedin"></i>
+                        </a>
+                    </div> 
                 </div>
             </div>
         )
--- portfolio-site/src/containers/Navbar.js
+++ portfolio-site/src/containers/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
     render(){
         return (
             <React.Fragment >
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav id="my-nav-bar" class="navbar navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand" href="/">{'< Sergio />'}</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
